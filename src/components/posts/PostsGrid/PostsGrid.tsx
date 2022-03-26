1d178374a5aeda348720209a2db43f9388434a45
Stavros <snikoletopoulos@hotmail.com>
2022-03-26 15:29:32 +0200

    Create PostGrid component

---
+++ src/components/posts/PostsGrid/PostsGrid.tsx
@@ -0,0 +1,28 @@
+import styles from "./PostsGrid.module.css";
+
+import { Post } from "types/post.types";
+
+import PostItem from "components/posts/PostItem/PostItem";
+
+type Props = {
+	posts: Post[];
+};
+
+const PostsGrid: React.FC<Props> = props => {
+	return (
+		<ul className={styles.grid}>
+			{props.posts.map(post => (
+				<PostItem
+					key={post.id}
+					title={post.title}
+					date={post.date}
+					excerpt={post.excerpt}
+					image={post.image}
+					slug={post.slug}
+				/>
+			))}
+		</ul>
+	);
+};
+
+export default PostsGrid;
