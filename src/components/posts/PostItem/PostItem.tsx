c78b1b6c7586332bd609b8392486e78b9835c494
Stavros <snikoletopoulos@hotmail.com>
2022-03-26 15:33:14 +0200

    Create PostItem component

---
+++ src/components/posts/PostItem/PostItem.tsx
@@ -0,0 +1,46 @@
+import styles from "./PostItem.module.css";
+import Link from "next/link";
+import Image from "next/image";
+
+import { Post } from "types/post.types";
+
+interface Props {
+	title: Post["title"];
+	image: Post["image"];
+	excerpt: Post["excerpt"];
+	date: Post["date"];
+	slug: string;
+}
+
+const PostItem: React.FC<Props> = props => {
+	const { title, image, excerpt, date, slug } = props;
+
+	const formattedDate = new Date(date).toLocaleDateString("en-US", {
+		day: "numeric",
+		month: "long",
+		year: "numeric",
+	});
+
+	const imagePath = `/images/posts/${slug}/${image}`;
+	const linkPath = `/posts/${slug}`;
+
+	return (
+		<li className={styles.post}>
+			<Link href={linkPath}>
+				<a>
+					<div className={styles.image}>
+						<Image src={imagePath} alt={title} height={300} width={200} />
+					</div>
+
+					<div className={styles.content}>
+						<h3>{title}</h3>
+						<time>{formattedDate}</time>
+						<p>{excerpt}</p>
+					</div>
+				</a>
+			</Link>
+		</li>
+	);
+};
+
+export default PostItem;
