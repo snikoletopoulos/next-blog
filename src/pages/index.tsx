166eb4489cd1d8d1a48d7fb34f9f81292365da2e
Stavros <snikoletopoulos@hotmail.com>
2022-03-26 15:33:43 +0200

    Render FeaturePosts component with dummy data

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,45 @@
 import type { NextPage } from "next";
 
+import { Post } from "types/post.types";
+
 import Hero from "components/home-page/Hero/Hero";
+import FeaturedPosts from "components/home-page/FeaturedPosts/FeaturedPosts";
+
+const DUMMY_POSTS: Post[] = [
+	{
+		id: 1,
+		slug: "getting-started-with-nextjs",
+		title: "Getting Started with Next.js",
+		excerpt:
+			"NextJS is tje React framework for production - it makes fullstack React apps and sites a breeze and ships with build-in SSR.",
+		image: "getting-started-with-nextjs.jpg",
+		date: "2022-02-10",
+	},
+	{
+		id: 2,
+		slug: "getting-started-with-nextjs2",
+		title: "Getting Started with Next.js",
+		excerpt:
+			"NextJS is tje React framework for production - it makes fullstack React apps and sites a breeze and ships with build-in SSR.",
+		image: "getting-started-with-nextjs.jpg",
+		date: "2022-02-10",
+	},
+	{
+		id: 3,
+		slug: "getting-started-with-nextjs3",
+		title: "Getting Started with Next.js",
+		excerpt:
+			"NextJS is tje React framework for production - it makes fullstack React apps and sites a breeze and ships with build-in SSR.",
+		image: "getting-started-with-nextjs.jpg",
+		date: "2022-02-10",
+	},
+];
 
 const HomePage: NextPage = () => {
 	return (
 		<>
 			<Hero />
-			{/* <FeaturedPosts /> */}
+			<FeaturedPosts posts={DUMMY_POSTS} />
 		</>
 	);
 };
