3e0c1673c7987ce532aa478d228f8f1557704075
Stavros <snikoletopoulos@hotmail.com>
2022-03-26 15:28:47 +0200

    Add structure to feature posts component

--- src/components/home-page/FeaturedPosts/FeaturedPosts.tsx
+++ src/components/home-page/FeaturedPosts/FeaturedPosts.tsx
@@ -1,7 +1,20 @@
-interface Props {}
+import styles from "./FeaturedPosts.module.css";
+
+import { Post } from "types/post.types";
+
+import PostsGrid from "components/posts/PostsGrid/PostsGrid";
+
+type Props = {
+	posts: Post[];
+};
 
 const FeaturedPosts: React.FC<Props> = props => {
-	return <div></div>;
+	return (
+		<section className={styles.latest}>
+			<h2>Featured Posts</h2>
+			<PostsGrid posts={props.posts} />
+		</section>
+	);
 };
 
 export default FeaturedPosts;
