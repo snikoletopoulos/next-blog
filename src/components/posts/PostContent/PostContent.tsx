27ac070acea2fadc63f2ec38762d132bf3b1237c
Stavros <snikoletopoulos@hotmail.com>
2022-03-28 02:39:33 +0300

    Use next image in react markdown

--- src/components/posts/PostContent/PostContent.tsx
+++ src/components/posts/PostContent/PostContent.tsx
@@ -1,4 +1,5 @@
 import styles from "./PostContent.module.css";
+import Image from "next/image";
 
 import ReactMarkdown from "react-markdown";
 import { Post } from "types/post.types";
@@ -18,10 +19,23 @@ const PostContent: React.FC<Props> = props => {
 
 	const imagePath = `/images/posts/${post.slug}/${post.image}`;
 
+	const customRenderers: ReactMarkdownRenderers = {
+		img(image) {
+			return (
+				<Image
+					src={`/images/posts/${post.slug}/${image.src}`}
+					alt={image.alt}
+					width={600}
+					height={300}
+				/>
+			);
+		},
+	};
+
 	return (
 		<article className={styles.content}>
 			<PostHeader title={post.title} image={imagePath} />
-			<ReactMarkdown>{post.content}</ReactMarkdown>
+			<ReactMarkdown components={customRenderers}>{post.content}</ReactMarkdown>
 		</article>
 	);
 };
