739214835b612bd0cb0a4a444a9bc93e107f1dab
Stavros <snikoletopoulos@hotmail.com>
2022-03-27 14:57:21 +0300

    Get post with getStaticProps

--- src/pages/posts/[slug].tsx
+++ src/pages/posts/[slug].tsx
@@ -1,9 +1,51 @@
-import type { NextPage } from "next";
+import type { GetStaticPaths, GetStaticProps, NextPage } from "next";
+
+import { Post } from "types/post.types";
+import { ParsedUrlQuery } from "querystring";
+import { getPostData, getPostsFiles } from "helpers/posts-utils";
 
 import PostContent from "components/posts/PostContent/PostContent";
 
-const PostDetailPage: NextPage = () => {
-	return <PostContent />;
+interface Params extends ParsedUrlQuery {
+	slug: string;
+}
+
+interface StaticProps {
+	post: Post;
+}
+
+const PostDetailPage: NextPage<StaticProps> = props => {
+	return <PostContent post={props.post} />;
 };
 
 export default PostDetailPage;
+
+export const getStaticProps: GetStaticProps<
+	StaticProps,
+	Params
+> = async context => {
+	const { params } = context;
+
+	if (!params?.slug) {
+		throw new Error("Missing slug");
+	}
+
+	const post = getPostData(params.slug);
+
+	return {
+		props: {
+			post,
+		},
+	};
+};
+
+export const getStaticPaths: GetStaticPaths<Params> = async () => {
+	const postsFileNames = getPostsFiles();
+
+	const slugs = postsFileNames.map(fileName => fileName.replace(/\.md$/, ""));
+
+	return {
+		paths: slugs.map(slug => ({ params: { slug } })),
+		fallback: false,
+	};
+};
