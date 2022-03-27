b3230426412d53413eec7755131003e0dafa469b
Stavros <snikoletopoulos@hotmail.com>
2022-03-27 06:20:01 +0300

    Get post data from props

--- src/components/posts/PostContent/PostContent.tsx
+++ src/components/posts/PostContent/PostContent.tsx
@@ -1,27 +1,23 @@
 import styles from "./PostContent.module.css";
 
 import ReactMarkdown from "react-markdown";
+import { Post } from "types/post.types";
 
 import PostHeader from "components/posts/PostHeader/PostHeader";
 
-const DUMMY_POST = {
-	id: 1,
-	slug: "getting-started-with-nextjs",
-	title: "Getting Started with Next.js",
-	image: "getting-started-nextjs.png",
-	date: "2022-02-10",
-	content: "# This is a first post",
-};
-
-interface Props {}
+interface Props {
+	post: Post;
+}
 
 const PostContent: React.FC<Props> = props => {
-	const imagePath = `/images/posts/${DUMMY_POST.slug}/${DUMMY_POST.image}`;
+	const { post } = props;
+
+	const imagePath = `/images/posts/${post.slug}/${post.image}`;
 
 	return (
 		<article className={styles.content}>
-			<PostHeader title={DUMMY_POST.title} image={imagePath} />
-			<ReactMarkdown>{DUMMY_POST.content}</ReactMarkdown>
+			<PostHeader title={post.title} image={imagePath} />
+			<ReactMarkdown>{post.content}</ReactMarkdown>
 		</article>
 	);
 };
