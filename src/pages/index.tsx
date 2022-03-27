1c08e9e9e50c97aa14f8f26a14de9c8b2f488be6
Stavros <snikoletopoulos@hotmail.com>
2022-03-27 04:48:53 +0200

    Fetch posts from file system instead of dummy data

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,47 +1,32 @@
-import type { NextPage } from "next";
+import type { NextPage, GetStaticProps } from "next";
 
 import { Post } from "types/post.types";
+import { getFeaturedPosts } from "helpers/posts-utils";
 
 import Hero from "components/home-page/Hero/Hero";
 import FeaturedPosts from "components/home-page/FeaturedPosts/FeaturedPosts";
 
-export const DUMMY_POSTS: Post[] = [
-	{
-		id: 1,
-		slug: "getting-started-with-nextjs",
-		title: "Getting Started with Next.js",
-		excerpt:
-			"NextJS is tje React framework for production - it makes fullstack React apps and sites a breeze and ships with build-in SSR.",
-		image: "getting-started-nextjs.png",
-		date: "2022-02-10",
-	},
-	{
-		id: 2,
-		slug: "getting-started-with-nextjs",
-		title: "Getting Started with Next.js",
-		excerpt:
-			"NextJS is tje React framework for production - it makes fullstack React apps and sites a breeze and ships with build-in SSR.",
-		image: "getting-started-nextjs.png",
-		date: "2022-02-10",
-	},
-	{
-		id: 3,
-		slug: "getting-started-with-nextjs",
-		title: "Getting Started with Next.js",
-		excerpt:
-			"NextJS is tje React framework for production - it makes fullstack React apps and sites a breeze and ships with build-in SSR.",
-		image: "getting-started-nextjs.png",
-		date: "2022-02-10",
-	},
-];
+interface StaticProps {
+	posts: Post[];
+}
 
-const HomePage: NextPage = () => {
+const HomePage: NextPage<StaticProps> = props => {
 	return (
 		<>
 			<Hero />
-			<FeaturedPosts posts={DUMMY_POSTS} />
+			<FeaturedPosts posts={props.posts} />
 		</>
 	);
 };
 
 export default HomePage;
+
+export const getStaticProps: GetStaticProps<StaticProps> = async () => {
+	const featuredPosts = getFeaturedPosts();
+
+	return {
+		props: {
+			posts: featuredPosts,
+		},
+	};
+};
