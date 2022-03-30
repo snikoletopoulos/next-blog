fa009ebde6a21fe732edde5542dde8b4704c0425
Stavros <snikoletopoulos@hotmail.com>
2022-03-31 01:37:34 +0300

    Render Notification in ContactForm based on message state

--- src/components/ContactForm/ContactForm.tsx
+++ src/components/ContactForm/ContactForm.tsx
@@ -1,22 +1,81 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import styles from "./ContactForm.module.css";
 
-import axios from "axios";
+import { sendMessage } from "services/contact";
+
+import Notification from "components/ui/Notification/Notification";
+
+type RequestStatus = "pending" | "success" | "error";
+
+export interface NotificationObject {
+	status: RequestStatus;
+	title: string;
+	message: string;
+}
 
 const ContactForm: React.FC = () => {
 	const [email, setEmail] = useState("");
 	const [name, setName] = useState("");
 	const [message, setMessage] = useState("");
 
-	const handleMessageSend: React.FormEventHandler<HTMLFormElement> = event => {
+	const [requestStatus, setRequestStatus] = useState<RequestStatus | null>(
+		null
+	);
+	const [requestError, setRequestError] = useState<string>("");
+
+	const handleMessageSend: React.FormEventHandler<
+		HTMLFormElement
+	> = async event => {
 		event.preventDefault();
 
-		axios.post("/api/contact", {
-			name,
-			email,
-			message,
-		});
+		setRequestStatus("pending");
+
+		try {
+			await sendMessage({
+				email,
+				name,
+				message,
+			});
+
+			setRequestStatus("success");
+		} catch (error) {
+			if (error instanceof Error) {
+				setRequestError(
+					error.message ?? "Your message failed to send. Please try again."
+				);
+			} else {
+				setRequestError("An unexpected error occurred.");
+			}
+
+			setRequestStatus("error");
+		}
 	};
+
+	let notification: NotificationObject | null = null;
+
+	if (requestStatus === "pending") {
+		notification = {
+			status: "pending",
+			title: "Sending message...",
+			message: "Your message is on its way!",
+		};
+	}
+
+	if (requestStatus === "success") {
+		notification = {
+			status: "success",
+			title: "Message sent!",
+			message: "Your message was sent successfully!",
+		};
+	}
+
+	if (requestStatus === "error") {
+		notification = {
+			status: "error",
+			title: "Message failed!",
+			message: requestError,
+		};
+	}
 
 	return (
 		<section className={styles.contact}>
@@ -60,6 +119,13 @@ const ContactForm: React.FC = () => {
 					<button>Send Message</button>
 				</div>
 			</form>
+			{notification && (
+				<Notification
+					status={notification.status}
+					title={notification.title}
+					message={notification.message}
+				/>
+			)}
 		</section>
 	);
 };
