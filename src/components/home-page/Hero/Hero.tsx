bae67e24ef4f9d533f87ef9b71fd64dc9d5610a9
Stavros <snikoletopoulos@hotmail.com>
2022-03-30 03:24:59 +0300

    Format code

--- src/components/home-page/Hero/Hero.tsx
+++ src/components/home-page/Hero/Hero.tsx
@@ -5,7 +5,12 @@ const Hero: React.FC = () => {
 	return (
 		<section className={styles["hero"]}>
 			<div className={styles["image"]}>
-				<Image src="/images/site/stavros.png" alt="Stavros" width={300} height={300} />
+				<Image
+					src="/images/site/stavros.png"
+					alt="Stavros"
+					width={300}
+					height={300}
+				/>
 			</div>
 			<h1>Hi, I'm Stavros</h1>
 			<p>
